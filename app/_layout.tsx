60572ddb5241b1e1a264ed853b539ad3738b6ce5
harsh1924 <harshrpanwar@gmail.com>
2026-03-07 15:02:22 +0530

    Done

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,23 @@
 import { Stack } from "expo-router";
 import "./globals.css";
 import { StatusBar } from "react-native";
+import * as NavigationBar from "expo-navigation-bar";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+
+  useEffect(() => {
+    NavigationBar.setVisibilityAsync("hidden");
+    NavigationBar.setBehaviorAsync("overlay-swipe");
+  }, []);
+
   return (
     <>
       <StatusBar hidden={true} />
       <Stack>
-        <Stack.Screen
-          name="(tabs)"
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="movies/[id]"
-          options={{ headerShown: false }}
-        />
-      </Stack></>
-  )
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+      </Stack>
+    </>
+  );
 }
